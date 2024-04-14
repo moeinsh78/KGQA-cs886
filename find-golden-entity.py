25f6c096c674403eb9f0baf45120c6896b0025a8
moeinsh78 <moeiiinsh@gmail.com>
2024-04-14 21:04:08 +0000

    implemented Distilbert embeddings, started graph construction

---
+++ find-golden-entity.py
@@ -0,0 +1,64 @@
+from transformers import DistilBertTokenizer, DistilBertModel
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import pandas as pd
+
+
+
+
+def find_query_embedding(query):
+    # Just to extract the named entity in the question
+    q = query[query.find("[") + 1: query.find("]")]
+    
+    tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+    model = DistilBertModel.from_pretrained("distilbert-base-uncased")
+    # encoded_input = tokenizer(query, return_tensors='pt')
+    encoded_input = tokenizer(q, return_tensors='pt', max_length=512, padding=True, truncation=True)
+    query_vector_embedding = model(**encoded_input).last_hidden_state[:, 0, :].detach().numpy()
+    return np.concatenate(query_vector_embedding, axis=0)
+
+
+def load_entity_embeddings():
+    entity_embeddings = np.load("entity_embeddings.npy")
+    print("Entity Embeddings Shape:", entity_embeddings.shape)
+    return entity_embeddings
+
+
+def load_entities_dict():
+    entities = pd.read_table("dataset/MetaQA/MetaQA-3/entity/kb_entity_dict.txt", delimiter = "\t", header=None)
+    entities = entities.rename(columns = {0: "entity_ids", 1: "entity_labels"})
+    entity_id_label_dict = entities.set_index('entity_ids').to_dict()['entity_labels']
+    entity_label_id_dict = entities.set_index('entity_labels').to_dict()['entity_ids']
+    return entity_id_label_dict, entity_label_id_dict
+
+
+
+def get_most_similar_entity_ids(n = 3):
+    entities_dict, entities_dict_reverse = load_entities_dict()
+
+    query_embedding = find_query_embedding(query = "[Mona McKinnon] appears in which film")
+    entity_embeddings = load_entity_embeddings()
+    
+    # Calculate cosine similarities between the query vector and the dataset
+    similarities = cosine_similarity(entity_embeddings, [query_embedding]).flatten()
+
+    print(similarities)
+    print(type(similarities))
+    
+    # Find the most similar vector(s)
+    # top_n_indices = np.argpartition(similarities, -n)[-n:]
+    top_n_indices = np.argsort(similarities)[-n:]
+    top_n_indices
+    print("Most Similar Indices:", top_n_indices)
+    print("Top Scores:", similarities[top_n_indices])
+    print("Most Similar Labels:")
+    top_n_indices[:] = top_n_indices[::-1]
+    labels = []
+    for ind in top_n_indices:
+        labels.append(entities_dict[ind])
+
+    print(labels)    
+    return top_n_indices, labels
+
+
+get_most_similar_entity_ids(n = 10)
--- txt2Vec.py
+++ txt2Vec.py
@@ -1,12 +1,63 @@
 from transformers import DistilBertTokenizer, DistilBertModel
 from tqdm import tqdm
-import pandas
+import pandas as pd
+import numpy as np
+
+
+
+def load_kb_dict():
+    entities = pd.read_table("dataset/MetaQA/MetaQA-3/entity/kb_entity_dict.txt", delimiter = "\t", header=None)
+    entities = entities.rename(columns = {0: "entity_ids", 1: "entity_labels"})
+    return entities
+
+
+entities = load_kb_dict()
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+model = DistilBertModel.from_pretrained("distilbert-base-uncased")
+
+
+batch_size = 32
+embeddings = []
+
+
+entities_dict = {k:v for v,k in enumerate(list(set(entities.entity_ids)))}
+entity_labels_list = list(entities.entity_labels)
+encoded_entity_labels = tokenizer.batch_encode_plus(entity_labels_list, return_tensors='pt', max_length=512, padding=True, truncation=True)
+
+
+# for i in tqdm(range(0, len(encoded_entity_labels['input_ids']), batch_size)):
+for i in tqdm(range(0, len(encoded_entity_labels['input_ids']), batch_size)):
+    batch_input_ids = encoded_entity_labels['input_ids'][i:i + batch_size]
+    batch_attention_mask = encoded_entity_labels['attention_mask'][i:i + batch_size]
+    
+    outputs = model(batch_input_ids, attention_mask=batch_attention_mask)
+    
+    batch_embeddings = outputs.last_hidden_state[:,0,:]
+    print(batch_embeddings)
+    batch_embeddings = batch_embeddings.detach().numpy()
+    # print(batch_embeddings.shape)
+
+#     positions = []
+#     for j in range(i, i + batch_size):
+#         if j >= len(entities):
+#             break
+#         key = entities_dict[entities.iloc[j]['entity_ids']]
+#         p = getPositionEncoding(key, d=768)
+#         positions.append(p)
+
+#     positional_encoding = np.concatenate(positions, axis = 0)
+#     batch_embeddings += positional_encoding
+    embeddings.append(batch_embeddings)
+
+embeddings = np.concatenate(embeddings, axis=0)
+
+np.save('./entity_embeddings.npy', embeddings)
+
+
 
 # text1 = "What does [Grégoire Colin] appear in"
 # encoded_input1 = tokenizer.batch_encode_plus(text1, return_tensors='pt')
 # output1 = model(**encoded_input1)
-
-# print(type(output1.last_hidden_state))
 
 
 # text2 = "What does [Grégoire Colin] appear in"
@@ -14,51 +65,3 @@ import pandas
 # output2 = model(**encoded_input2)
 
 # print(output2.last_hidden_state)
-
-batch_size = 32
-embeddings = []
-
-
-
-def encode_labels(entity_labels, tokenizer):
-    encoded = tokenizer.batch_encode_plus(entity_labels, return_tensors='pt', max_length=512, padding=True, truncation=True)
-    return encoded
-
-
-def load_kb_dict():
-    entity_labels = pandas.read_table("dataset/MetaQA/MetaQA-3/entity/kb_entity_dict.txt", delimiter = "\t", header=None)
-    entity_labels = entity_labels.rename(columns = {0: "entity_ids", 1: "entity_labels"})
-    return entity_labels
-
-
-
-entity_labels = load_kb_dict()
-
-tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
-model = DistilBertModel.from_pretrained("distilbert-base-uncased")
-
-
-encoded_entity_labels = encode_labels(entity_labels, tokenizer)
-print(encoded_entity_labels)
-
-for i in tqdm(range(0, len(encoded_entity_labels['entity_ids']), batch_size)):
-    batch_input_ids = encoded_entity_labels['input_ids'][i:i + batch_size]
-    batch_attention_mask = encoded_entity_labels['attention_mask'][i:i + batch_size]
-    
-    outputs = model(batch_input_ids, attention_mask=batch_attention_mask)
-    
-    batch_embeddings = outputs.last_hidden_state[:,0,:]
-    batch_embeddings = batch_embeddings.cpu().numpy()
-#     positions = []
-#     for j in range(i, i + batch_size):
-#         if j >= len(df):
-#             break
-#         k = videos_dict[df.iloc[j]['video_id']]
-#         p = getPositionEncoding(k, d=768)
-#         positions.append(p)
-
-#     positional_encoding = np.concatenate(positions, axis = 0)
-#     batch_embeddings += positional_encoding
-#     embeddings.append(batch_embeddings)
-
-# embeddings = np.concatenate(embeddings, axis=0)
